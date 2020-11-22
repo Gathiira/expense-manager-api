af9de6d43301d90790e92b514d6f049c6c65734c
Gathiira <mwangyjose@gmail.com>
2020-11-22 18:47:32 +0300

    added media and static root

--- expensemanager/settings.py
+++ expensemanager/settings.py
@@ -192,6 +192,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
